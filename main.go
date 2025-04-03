399df5383cae514cab508261ac0e4c7b4088d7bf
Vinícius Morales <vinicius.morales@aluno.senai.br>
2025-04-03 10:00:40 -0300

    Exercício 2-completo

--- main.go
+++ main.go
@@ -3,9 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	var contagemRegressiva = [] int{90, 80, 70, 60, 50}
+	/*var contagemRegressiva = [] int{90, 80, 70, 60, 50}
 	contagemRegressiva = append(contagemRegressiva, 40, 30, 20)
 	fmt.Println(contagemRegressiva)
 	fmt.Println(len(contagemRegressiva), cap(contagemRegressiva))
-
+*/
+	var nomes = [] string{"Lucas", "João", "Sabrina", "Maria", "Ana"}
+	fmt.Println(nomes [0:2])
+	fmt.Println(nomes [3:5])
+	rangeNomedoMeio := nomes [2]
+	fmt.Println(rangeNomedoMeio)
 }
