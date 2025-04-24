9baa6a4fb50619a1a518eba0ceb00e78d2d0e2b5
Vinícius Morales <vinicius.morales@aluno.senai.br>
2025-04-24 14:34:16 -0300

    Contabilizar vendas da Cantina

--- main.go
+++ main.go
@@ -3,32 +3,21 @@ package main
 import "fmt"
 
 func main() {
-	capitais := map[string]string{
-		"SP": "São Paulo",
-		"RJ": "Rio de Janeiro",
-		"MG": "Minas Gerais",
-		"BH": "Bahia",
-	}
-
-	capitais["PR"] = "Paraná"
-
-	for sigla, capital := range capitais {
-		fmt.Printf("A capital do estado %s é %s\n", sigla, capital)
-	}
-	delete(capitais, "BH")
-	fmt.Println("Após a remoção de BH:")
-	for sigla, capital := range capitais {
-		fmt.Printf("A capital do estado %s é %s\n", sigla, capital)
-	}
-
-	matiz := [3][3]int{
-		{1, 2, 3},
-		{4, 5, 6},
-		{7, 8, 9},
-	}
-	for _, linha := range matiz {
-		fmt.Println(linha)
-	}
 	
+	produtosCantina := map[string]int{
+		"Coxinha": 10,
+		"Pão de Queijo": 15,
+		"Refrigerante":  20,
+	}
+	 for produto, quantias := range produtosCantina {
+		fmt.Printf("Produto: %s; Quantidade: %d\n", produto, quantias)
+	}
+	produtosCantina["Coxinha"] -= 2
+	produtosCantina["Pão de Queijo"] -= 1
+
+	fmt.Println("Após a venda:")
+	for produto, quantias := range produtosCantina {
+		fmt.Printf("Produto: %s; Quantidade: %d\n", produto, quantias)
+	}
 }
 
